ec3723d52a560928df4aeab2310326af9c2e9cf3
VycMajoris <ok1907@gmail.com>
2023-02-23 21:30:12 +0300

    add restart logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-restricted-syntax */
 const gameBoardGrid = document.querySelector(".gameBoardGrid");
 const currentPlayerText = document.querySelector(".currentPlayerText");
+const restartBtn = document.querySelector(".restartBtn");
 
 // ------------------------------- Gameboard -------------------------------
 const GameBoard = (() => {
@@ -43,39 +44,101 @@ const Players = (letter) => {
 const playerX = Players("x");
 const playerO = Players("o");
 
+// ------------------------------- Game Over Logic --------------------------
+
+const isGameOver = () => {
+  let isOver = false;
+};
+
 // ------------------------------- Game Logic -------------------------------
 
-const GameLogic = ((choiceMade, isCellEmpty) => {
+const GameLogic = (() => {
   /* let currentPlayer = playerX.getLetter(); */
+
   let currentPlayer = playerX.getLetter();
-  console.log(currentPlayer);
 
   const writeLetterToBoard = (e) => {
-    if (e.target.innerHTML === "")
+    if (e.target.innerHTML === "" && isGameOver.isOver === false)
       GameBoard.cellContent.splice(e.target.id, 1, currentPlayer.toUpperCase());
-    console.log(e.target.id);
+
     displayBoard(GameBoard);
+
+    if (checkForWin(GameBoard.cellContent) !== "") {
+      if (checkForWin(GameBoard.cellContent) === "tie") {
+        currentPlayerText.innerHTML = "It's a tie!";
+        isGameOver.isOver = true;
+      } else {
+        currentPlayerText.innerHTML = `Player ${checkForWin(
+          GameBoard.cellContent
+        )} Wins!`;
+        isGameOver.isOver = true;
+      }
+    }
   };
 
   const setPlayer = (e) => {
-    if (e.target.innerHTML === "") {
-      if (currentPlayer === playerX.getLetter()) {
-        currentPlayerText.innerHTML = "Player O's Turn";
-        writeLetterToBoard(e);
-        currentPlayer = playerO.getLetter();
-      } else {
-        currentPlayerText.innerHTML = "Player X's Turn";
-        writeLetterToBoard(e);
-        currentPlayer = playerX.getLetter();
+    if (isGameOver.isOver === false) {
+      if (e.target.innerHTML === "") {
+        if (currentPlayer === playerX.getLetter()) {
+          currentPlayerText.innerHTML = "Player O's Turn";
+          console.log(currentPlayer);
+          writeLetterToBoard(e);
+          currentPlayer = playerO.getLetter();
+        } else {
+          currentPlayerText.innerHTML = "Player X's Turn";
+          console.log(currentPlayer);
+          writeLetterToBoard(e);
+          currentPlayer = playerX.getLetter();
+        }
       }
     }
-    console.log(`cellContent is ${GameBoard.cellContent}`);
+
     console.log(GameBoard.cellContent);
   };
 
   const getPlayer = () => currentPlayer;
 
-  const checkIfOver = () => {};
+  // Check if the game is over
+
+  function checkForWin(board) {
+    // Check rows
+    for (let i = 0; i < 9; i += 3) {
+      if (
+        board[i] === board[i + 1] &&
+        board[i + 1] === board[i + 2] &&
+        board[i] !== ""
+      ) {
+        return board[i];
+      }
+    }
+
+    // Check columns
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[i] === board[i + 3] &&
+        board[i + 3] === board[i + 6] &&
+        board[i] !== ""
+      ) {
+        return board[i];
+      }
+    }
+
+    // Check diagonals
+    if (board[0] === board[4] && board[4] === board[8] && board[0] !== "") {
+      return board[0];
+    }
+    if (board[2] === board[4] && board[4] === board[6] && board[2] !== "") {
+      return board[2];
+    }
+
+    // Check if all squares are filled
+    if (board.every((square) => square !== "")) {
+      return "tie";
+    }
+
+    // If no win or tie, game is not over
+    return "";
+  }
 
   return { setPlayer, getPlayer, writeLetterToBoard };
 })();
@@ -91,3 +154,17 @@ for (let i = 0; i < 9; i++) {
   /* gameBoardCell.addEventListener("click", GameLogic.writeLetterToBoard); */
   gameBoardGrid.append(gameBoardCell);
 }
+
+// restart button to clear the logic
+
+function restartGame() {
+  GameBoard.cellContent = ["", "", "", "", "", "", "", "", ""];
+  displayBoard(GameBoard);
+
+  /* GameLogic.isOver = false;
+  GameLogic.currentPlayer = playerX.getLetter(); */
+
+  currentPlayerText.innerHTML = "Player X's Turn";
+}
+
+restartBtn.addEventListener("click", restartGame);
